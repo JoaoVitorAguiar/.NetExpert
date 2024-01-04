0e72c7ba42ad99ce99d7b344995e129f0eec5df7
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2024-01-04 16:30:05 -0300

    feat: add user controller

---
+++ FormacaoAspNetCore/DevFreela/DevFreela.API/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Models;
+
+[ApiController]
+[Route("api/users")]
+public class UserController : ControllerBase
+{
+    // api/users/1
+    [HttpGet("{id:int}")]
+    public IActionResult GetById(int id)
+    {
+        return Ok();
+    }
+
+    //api/user
+    [HttpPost]
+    public IActionResult Register(
+        [FromBody] CreateUserModel createUserModel)
+    {
+        return CreatedAtAction(nameof(GetById), new { id = 1 }, createUserModel);
+    }
+
+    // api/users/1/login
+    [HttpPut("{id:int}/login")]
+    public IActionResult Login(int id, LoginModel loginModel)
+    {
+        return NoContent();
+    }
+}
