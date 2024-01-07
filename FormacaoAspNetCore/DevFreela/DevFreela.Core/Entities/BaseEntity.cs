9f60966eef4913163167c27c2a5abf835da81207
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2024-01-07 15:05:03 -0300

    feat: add base entity

---
+++ FormacaoAspNetCore/DevFreela/DevFreela.Core/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace DevFreela.Core.Entities;
+
+public abstract class BaseEntity
+{
+    // Um construtor protegido significa que apenas
+    // membros derivados podem construir instâncias
+    // da classe (e instâncias derivadas) usando esse construtor
+    protected BaseEntity() { }
+    public int Id { get; private set; }
+}
