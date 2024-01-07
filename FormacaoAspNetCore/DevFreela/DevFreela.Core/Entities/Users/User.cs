ca5993ce5588f11e4ac16f7ef52f935bf683109a
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2024-01-07 15:05:40 -0300

    feat: add user entity

---
+++ FormacaoAspNetCore/DevFreela/DevFreela.Core/Entities/Users/User.cs
@@ -0,0 +1,31 @@
+﻿using DevFreela.Core.Entities.Projects;
+
+namespace DevFreela.Core.Entities.Users;
+
+class User : BaseEntity
+{
+    public User(string fisrtName, string lastName, string email, DateTime birthDate)
+    {
+        FisrtName = fisrtName;
+        LastName = lastName;
+        Email = email;
+        BirthDate = birthDate;
+
+        CreatedAt = DateTime.Now;
+        Active = true;
+        Skills = [];
+        FreelanceProject = [];
+        OwnedProject = [];
+    }
+
+    public string FisrtName { get; private set; }
+    public string LastName { get; private set; }
+    public string Email { get; private set; }
+    public DateTime BirthDate { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public bool Active { get; private set; }
+
+    public List<UserSkill> Skills { get; private set; }
+    public List<Project> FreelanceProject { get; private set; }
+    public List<Project> OwnedProject { get; private set; }
+}
---
+++ FormacaoAspNetCore/DevFreela/DevFreela.Core/Entities/Users/UserSkill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities.Users;
+
+public class UserSkill: BaseEntity
+{
+    public UserSkill(int userId, int skillId)
+    {
+        UserId = userId;
+        SkillId = skillId;
+    }
+
+    public int UserId { get; private set; }
+    public int SkillId { get; private set; }
+}
