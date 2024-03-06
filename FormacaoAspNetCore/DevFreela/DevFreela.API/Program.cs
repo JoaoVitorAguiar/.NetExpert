238669487f212481f739a0f41aa0646972a494c8
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2024-03-06 17:24:11 -0300

    feat: add service DbContext in Program.cs

--- FormacaoAspNetCore/DevFreela/DevFreela.API/Program.cs
+++ FormacaoAspNetCore/DevFreela/DevFreela.API/Program.cs
@@ -1,14 +1,16 @@
 using DevFreela.Application.Services.Implementations;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Infrastructure.Persistense;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddSingleton<DevFreelaDbContext>();
 builder.Services.AddControllers();
 builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddDbContext<DevFreelaDbContext>(
+    options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
