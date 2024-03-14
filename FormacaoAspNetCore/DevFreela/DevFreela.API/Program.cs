ee720bc57b9a1947f1e947bb3a02175cebdad102
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-03-14 12:14:42 -0300

    feat: add mediatR and create project command

--- FormacaoAspNetCore/DevFreela/DevFreela.API/Program.cs
+++ FormacaoAspNetCore/DevFreela/DevFreela.API/Program.cs
@@ -1,6 +1,8 @@
+using DevFreela.Application.Commands.CreateProject;
 using DevFreela.Application.Services.Implementations;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Infrastructure.Persistense;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,8 +11,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<IUserServices, UserService>();
+builder.Services.AddScoped<ISkillsService, SkillService>();
+
+// Busca todas as classes neesse assembly que o mediator especifica para o padrão CQRS,
+// Ou seja, busca todas as classes que implementam o IRquest<> e assicia ao IRequestHandler<>
+builder.Services.AddMediatR(typeof(CreateProjectCommand));
+
+
 builder.Services.AddDbContext<DevFreelaDbContext>(
     options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ FormacaoAspNetCore/DevFreela/DevFreela.Application/Commands/CreateProject/CreateProjectHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Commands.CreateProject;
+
+// Trata as informações e realiza efetivamente a persistência dos dados
+public class CreateProjectHandler : IRequestHandler<CreateProjectCommand, int>
+{
+    public Task<int> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
