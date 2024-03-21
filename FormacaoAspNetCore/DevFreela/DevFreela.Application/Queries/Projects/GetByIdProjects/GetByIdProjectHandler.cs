fbe377b3c96667c5606403d8ce2df81484e8ada8
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-03-21 13:16:35 -0300

    feat: add queries in projects

---
+++ FormacaoAspNetCore/DevFreela/DevFreela.Application/Queries/Projects/GetByIdProjects/GetByIdProjectHandler.cs
@@ -0,0 +1,43 @@
+﻿using DevFreela.Application.ViewModel;
+using DevFreela.Infrastructure.Persistense;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Queries.Projects.GetByIdProjects;
+
+public class GetByIdProjectHandler : IRequestHandler<GetByIdProjectQuery, ProjectDetailsViewModel>
+{
+    private readonly DevFreelaDbContext _dbContext;
+
+    public GetByIdProjectHandler(DevFreelaDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<ProjectDetailsViewModel> Handle(GetByIdProjectQuery request, CancellationToken cancellationToken)
+    {
+        var project = await _dbContext.Projects
+    .Include(p => p.Client)
+    .Include(p => p.Freelancer)
+    .SingleOrDefaultAsync(p => p.Id == request.Id);
+        if (project == null) return null;
+
+        return new ProjectDetailsViewModel(
+            project.Id,
+            project.Title,
+            project.Description,
+            project.TotalCost,
+            project.StartedAt,
+            project.FinishedAt,
+            project.Client.FisrtName,
+            project.Client.LastName,
+            project.Freelancer.FisrtName,
+            project.Freelancer.LastName
+            );
+    }
+}
---
+++ FormacaoAspNetCore/DevFreela/DevFreela.Application/Queries/Projects/GetByIdProjects/GetByIdProjectQuery.cs
@@ -0,0 +1,19 @@
+﻿using DevFreela.Application.ViewModel;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Queries.Projects.GetByIdProjects;
+
+public class GetByIdProjectQuery : IRequest<ProjectDetailsViewModel>
+{
+    public GetByIdProjectQuery(int id)
+    {
+        Id = id;
+    }
+
+    public int Id { get; private set; }
+}
