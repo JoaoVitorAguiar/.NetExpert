215c4b0b2729c34df30a65280b03185677032d28
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2024-04-01 21:34:02 -0300

    feat: add create project command validation

--- FormacaoAspNetCore/DevFreela/DevFreela.API/Controllers/ProjectController.cs
+++ FormacaoAspNetCore/DevFreela/DevFreela.API/Controllers/ProjectController.cs
@@ -48,9 +48,13 @@ public class ProjectController : ControllerBase
     public async Task<IActionResult> Post(
         [FromBody] CreateProjectCommand command)
     {
-        if(command.Title.Length > 50)
+        if(!ModelState.IsValid)
         {
-            return BadRequest();
+            var messages = ModelState
+                .SelectMany(ms => ms.Value.Errors)
+                .Select(e => e.ErrorMessage)
+                .ToList();
+            return BadRequest(messages);
         }
 
         var id = await _mediator.Send(command);
---
+++ FormacaoAspNetCore/DevFreela/DevFreela.Application/Validators/CreateProjectCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using DevFreela.Application.Commands.Project.CreateProject;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Validators;
+
+public class CreateProjectCommandValidator : AbstractValidator<CreateProjectCommand>
+{
+    public CreateProjectCommandValidator()
+    {
+        RuleFor(p => p.Description)
+            .MaximumLength(255)
+            .WithMessage("O tamanho máximo de descrição é de 255 caracteres.");
+
+        RuleFor(p => p.Title)
+            .MaximumLength(30)
+            .WithMessage("O tamanho máximo de título é de 30 caracteres.");
+    }
+}
