5fed48739907fc37e459f09cfbc2b57f907326c5
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-04-27 21:06:21 -0300

    feat: feat: add unit test in create projects command handler

---
+++ FormacaoAspNetCore/DevFreela/DevFreela.UnitTests/Application/Commands/CreateProjectCommandHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿
+
+using DevFreela.Application.Commands.Project.CreateProject;
+using DevFreela.Core.Entities.Projects;
+using DevFreela.Core.Repositories;
+using Moq;
+
+namespace DevFreela.UnitTests.Application.Commands;
+
+public class CreateProjectCommandHandlerTests
+{
+    [Fact]
+    // Given_when_then
+    public async Task InputeDataOk_Executed_ReturnProjectId()
+    {
+        // Arrange
+        var projectRepository = new Mock<IProjectRepository>();
+        var createProjectCommand = new CreateProjectCommand 
+        { 
+            Title = "Teste",
+            Description = "Descrição Teste",
+            ClientId = 1,
+            FreelancerId = 1,
+            TotalCost = 5000
+        };
+
+        var createProjectCommandHandler = new CreateProjectHandler(projectRepository.Object);
+        
+        // Act
+        var id = await createProjectCommandHandler.Handle(createProjectCommand, new CancellationToken());
+
+
+        // Assert
+
+        Assert.True(id >= 0);
+        projectRepository.Verify(pr => pr.CreateProjectAsync(It.IsAny<Project>()), Times.Once);
+        
+    }
+}
