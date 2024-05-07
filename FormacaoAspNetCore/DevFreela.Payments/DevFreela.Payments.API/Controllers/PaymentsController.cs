9771cc640a2ea309d2b643038f5580b84cc8498e
João Vitor Aguiar <joaovitor4guiar@gmail.com>
2024-05-07 10:39:36 -0300

    feat: add microservices Payment API

---
+++ FormacaoAspNetCore/DevFreela.Payments/DevFreela.Payments.API/Controllers/PaymentsController.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Payments.API.Models;
+using DevFreela.Payments.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.Payments.API.Controllers;
+
+[ApiController]
+[Route("api/payments")]
+public class PaymentsController : ControllerBase
+{
+    private readonly IPaymentService _servicePayment;
+
+    public PaymentsController(IPaymentService servicePayment)
+    {
+        _servicePayment = servicePayment;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] PaymentInfoInputModel paymentInfo)
+    {
+        var result = await _servicePayment.Process(paymentInfo);
+
+        if(!result)
+        {
+            BadRequest();
+        }
+
+        return NoContent();
+    }
+}
